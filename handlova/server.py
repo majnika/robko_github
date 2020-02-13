9975d5ab01ace70fce69733c99a481e64e6edfc6
Filip Štrba <filipko.strba@gmail.com>
2020-02-13 11:58:25 +0100

    basic server/client

--- handlova/server.py
+++ handlova/server.py
@@ -6,6 +6,12 @@ addr = ("0.0.0.0", 12345)
 
 sock.bind(addr)
 
-sock.recv(1518)
+while True:
+    client = sock.recvfrom(1518)
+    mesage_bytes = client[0]
+    client_addr = client[1]
+
+    print("Message: " + mesage_bytes.decode("utf-8"))
+    print("From: " + client_addr[0] + ":" + str(client_addr[1]))
 
 sock.close()
